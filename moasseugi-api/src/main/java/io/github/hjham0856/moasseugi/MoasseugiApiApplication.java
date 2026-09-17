ff9cdad7b878a661965c6ed383c3dccafba67381
Hyeongjun Ham <hjham0856@gmail.com>
2026-09-18 07:47:14 +0900

    initial commit

---
+++ moasseugi-api/src/main/java/io/github/hjham0856/moasseugi/MoasseugiApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.hjham0856.moasseugi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoasseugiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoasseugiApiApplication.class, args);
+	}
+
+}
---
+++ moasseugi-api/src/test/java/io/github/hjham0856/moasseugi/MoasseugiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hjham0856.moasseugi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoasseugiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
